b5bbfdbd02722cb20d8ad79111d7cd9a08c689d6
Shalini <shalini16jha@gmail.com>
2023-03-20 14:46:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,123 @@
+console.log("Welcome to Spotify");
+
+//Initialize the variables
+let songIndex = 0; //Initilialy zero songs will be played
+let audioElement = new Audio("./song/1.mp3");
+let masterPlay = document.getElementById("masterPlay");
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
+let masterSongName = document.getElementById("masterSongName");
+let songItems = Array.from(document.getElementsByClassName("songItem"));
+
+let songs = [
+  {
+    songName: "Yeh tara woh tara",
+    filepath: "./song/1.mp3",
+    coverPath: "./image/yehtarawohtara.jpg_large",
+  },
+  {
+    songName: "Pal pal hai bhaari",
+    filepath: "./song/2.mp3",
+    coverPath: "./image/palPalHaiBhari.jpg",
+  },
+  {
+    songName: "Dekho na",
+    filepath: "./song/3.mp3",
+    coverPath: "./image/dekhoNa.jpg",
+  },
+  {
+    songName: "Saanwariya Saanwariya",
+    filepath: "./song/4.mp3",
+    coverPath: "./image/saanwariya.jpg",
+  },
+  {
+    songName: "Yun hi chala chal",
+    filepath: "./song/5.mp3",
+    coverPath: "./image/yuhiChalaChal.jpg",
+  },
+];
+
+songItems.forEach((element, i) => {
+  console.log(element, i);
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
+//audioElement.play()
+
+//Handle play/pause click
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-play-circle");
+    masterPlay.classList.add("fa-pause-circle");
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-pause-circle");
+    masterPlay.classList.add("fa-play-circle");
+    gif.style.opacity = 0;
+  }
+});
+
+//Listen To Events
+audioElement.addEventListener("timeupdate", () => {
+  //Update Seekbar
+  progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+  myProgressBar.value = progress;
+});
+myProgressBar.addEventListener("change", () => {
+  // (currentTime/total duration)*100=percentage
+  audioElement.currentTime =
+    (myProgressBar.value * audioElement.duration) / 100;
+});
+const makeAllPlays = () => {
+  Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+    (element) => {
+      element.classList.remove("fa-pause-circle");
+      element.classList.add("fa-play-circle");
+    }
+  );
+};
+Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+  (element) => {
+    element.addEventListener("click", (e) => {
+      makeAllPlays();
+      songIndex = parseInt(e.target.id);
+      e.target.classList.remove("fa-play-circle");
+      e.target.classList.add("fa-pause-circle");
+      audioElement.src = `./song/${songIndex + 1}.mp3`;
+      masterSongName.innerText=songs[songIndex].songName;
+      audioElement.currentTime = 0;
+      audioElement.play();
+      gif.style.opacity=1;
+      masterPlay.classList.remove("fa-play-circle");
+      masterPlay.classList.add("fa-pause-circle");
+    });
+  }
+);
+document.getElementById("next").addEventListener("click", () => {
+  if (songIndex >= 5) {
+    songIndex = 0;
+  } else {
+    songIndex += 1;
+  }
+  audioElement.src = `./song/${songIndex + 1}.mp3`;
+  masterSongName.innerText=songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove("fa-play-circle");
+  masterPlay.classList.add("fa-pause-circle");
+});
+document.getElementById("previous").addEventListener("click", () => {
+  if (songIndex <= 0) {
+    songIndex = 0;
+  } else {
+    songIndex -= 1;
+  }
+  audioElement.src = `./song/${songIndex + 1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove("fa-play-circle");
+  masterPlay.classList.add("fa-pause-circle");
+});
